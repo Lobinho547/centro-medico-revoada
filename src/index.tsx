9fd21eec26a69e9e4668cc118a7b29beed84cce8
Lobinho547 <yagogames547@gmail.com>
2025-05-10 14:26:02 -0300

    Remover conexão do MongoDB do frontend

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import connectDB from './database/connection';
-
-// Conectar ao banco de dados MongoDB
-connectDB().catch(console.error);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
